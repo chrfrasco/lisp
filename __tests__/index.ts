0e05de12974fd5c87469908b7446aaec9d98bce6
Christian Scott <christian.s@canva.com>
2020-03-21 21:43:21 +1100

    clean up mock assertions

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -5,23 +5,22 @@ test("runs correctly", () => {
   const print = jest.fn();
   const scope = Scope.forTesting(print);
   lisp(`(print (+ 1 1))`, scope);
-  expect(print.mock.calls.length).toEqual(1);
-  expect(print.mock.calls[0][0]).toEqual(2);
+  expect(print).toHaveBeenCalledWith(2);
 });
 
 test("multi-statement programs", () => {
   const print = jest.fn();
   const scope = Scope.forTesting(print);
   lisp(`(print "hello, ")\n(print "world")`, scope);
-  expect(print.mock.calls.length).toBe(2);
-  expect(print.mock.calls[0][0]).toEqual("hello, ");
-  expect(print.mock.calls[1][0]).toEqual("world");
+
+  expect(print).toHaveBeenCalledTimes(2);
+  expect(print).toHaveBeenNthCalledWith(1, "hello, ");
+  expect(print).toHaveBeenNthCalledWith(2, "world");
 });
 
 test("variable assignment", () => {
   const print = jest.fn();
   const scope = Scope.forTesting(print);
   lisp(`(def x 1)\n(print x)`, scope);
-  expect(print.mock.calls.length).toBe(1);
-  expect(print.mock.calls[0][0]).toEqual(1);
+  expect(print).toHaveBeenCalledWith(1);
 });
